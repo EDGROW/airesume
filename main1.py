6c90865115cc4d20f40888e2aafb78dc737e8f47
EDGROW <edgrow121@gmail.com>
2025-06-28 22:13:28 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -303,4 +303,5 @@ def clear():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Default for local
+    app.run(host="0.0.0.0", port=port, debug=False)
